20149f7b382e96db312b179ecb07b52018546ad9
GSAprod <92989496+GSAprod@users.noreply.github.com>
2024-06-07 14:42:00 +0100

    style: change size of location results, add result section element

---
+++ frontend/src/components/locationPicker/LocationPickerListSection.jsx
@@ -0,0 +1,5 @@
+export default function LocationPickerListSection() {
+  return (
+    <div className='mx-5 mt-4 text-xs font-bold text-white/50'>Recent</div>
+  )
+}
--- frontend/src/components/locationPicker/LocationSearchResult.jsx
+++ frontend/src/components/locationPicker/LocationSearchResult.jsx
@@ -3,8 +3,8 @@ import { string } from "prop-types"
 export default function LocationSearchResult({ name, description }) {
   return (
     <div className='px-5 py-2 my-1 hover:bg-black/20 cursor-pointer'>
-        <div className='font-bold text-lg'>{name}</div>
-        <div>{description}</div>
+        <div className='font-bold'>{name}</div>
+        <div className="text-sm">{description}</div>
     </div>
   )
 }
